17729dd95370905aa09fae68439cf4a9844c8047
Abdu Jemal <abduh9350@gmail.com>
2020-10-16 03:58:48 -0400

    google clone 50% completed

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Home.css';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar, IconButton } from '@material-ui/core';
+import Search from './Search';
+function Home() {
+    return (
+        <div className='home'>
+            <div className="home_header">
+                <div className="home_headerLeft">
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className="home_headerRight">
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    <IconButton>
+                        <AppsIcon />
+                    </IconButton>
+                    <Avatar />
+                </div>
+            </div>
+            <div className="home_body">
+                <img src='https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg' alt='' />
+                <div className="home_inputContainer">
+                    <Search hideButtons/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
